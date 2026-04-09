95fd2631c44dbb87df043f0c7cc6281c507f6520
YiorzHe <amirhafizi443@gmail.com>
2026-04-09 21:57:09 +0800

    Revert to mascot version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  adapter: node({ mode: 'standalone' }),
   server: {
     headers: {
       // Strict-Transport-Security: Force HTTPS for 1 year, include subdomains
